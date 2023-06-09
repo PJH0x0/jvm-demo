2f1c3b2e16da869153654a9fdd1f010b4e478cc3
pengjinghui <pengjinghui@xiaomi.com>
2023-06-09 19:16:25 +0800

    [Extends][TEST] Add pushOperandStack test
    Signed-off-by: pengjinghui <pengjinghui@xiaomi.com>

---
+++ tests/src/instructions_test/base_instructions_test.cpp
@@ -0,0 +1,34 @@
+#include <instructions/base/base_instructions.h>
+#include <rtda/heap/object.h>
+
+#include <gtest/gtest.h>
+
+
+namespace unit_test {
+class BaseInstructionsTest : public testing::Test {
+  protected:
+  BaseInstructionsTest() {}
+  ~BaseInstructionsTest() {}
+};
+TEST_F(BaseInstructionsTest, pushOperandStack_int) {
+  rtda::OperandStack operandStack(10);
+  instructions::pushOperandStack<int>(operandStack, 100);
+  EXPECT_EQ(operandStack.popInt(), 100);
+}
+TEST_F(BaseInstructionsTest, pushOperandStack_long) {
+  rtda::OperandStack operandStack(10);
+  instructions::pushOperandStack<long>(operandStack, 2997924580);
+  EXPECT_EQ(operandStack.popLong(), 2997924580);
+}
+TEST_F(BaseInstructionsTest, pushOperandStack_float) {
+  rtda::OperandStack operandStack(10);
+  instructions::pushOperandStack(operandStack, 3.1415926f);
+  EXPECT_EQ(operandStack.popFloat(), 3.1415926f);
+}
+TEST_F(BaseInstructionsTest, pushOperandStack_double) {
+  rtda::OperandStack operandStack(10);
+  instructions::pushOperandStack(operandStack, 2.71828182845);
+  EXPECT_EQ(operandStack.popDouble(), 2.71828182845);
+}
+
+}// namespace unit_test
